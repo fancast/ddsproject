fb3d53a3f34250e278f517514ce90a5a62551d26
Jerry Jean-Pierre <jeanpierrejerry@gmail.com>
2021-04-22 11:52:43 -0400

    Add files via upload

--- FromEnergyStorageModule/Publisher.cpp
+++ FromEnergyStorageModule/Publisher.cpp
@@ -174,7 +174,7 @@ ACE_TMAIN(int argc, ACE_TCHAR *argv[])
 	time(&current_time);
 	esm_signals.timestamp = asctime(localtime(&current_time));
 
-	//auto gtfpga = Gtfpga(PCIE_ADDRESS);
+	auto gtfpga = Gtfpga(PCIE_ADDRESS);
 
     for (int i = 0; i < 10; ++i) {
       DDS::ReturnCode_t error = esm_signals_writer->write(esm_signals, DDS::HANDLE_NIL);
