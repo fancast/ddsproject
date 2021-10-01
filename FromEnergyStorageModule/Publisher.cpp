80d17a24cf4e8135d9a1998fe0b444e68380c276
Jerry Jean-Pierre <jeanpierrejerry@gmail.com>
2021-10-01 13:57:03 -0400

    Add files via upload

---
+++ FromEnergyStorageModule/Publisher.cpp
@@ -0,0 +1,198 @@
+/* Publisher example derived from the EsmFeedbackSignals IDL example */
+
+#include <ace/Log_Msg.h>
+
+#include <dds/DdsDcpsInfrastructureC.h>
+#include <dds/DdsDcpsPublicationC.h>
+
+#include <dds/DCPS/Marked_Default_Qos.h>
+#include <dds/DCPS/Service_Participant.h>
+#include <dds/DCPS/WaitSet.h>
+
+#include <dds/DCPS/StaticIncludes.h>
+#ifdef ACE_AS_STATIC_LIBS
+#  include <dds/DCPS/RTPS/RtpsDiscovery.h>
+#  include <dds/DCPS/transport/rtps_udp/RtpsUdp.h>
+#endif
+
+#include "EsmFeedbackSignalsTypeSupportImpl.h"
+#include <iostream>
+
+// included for fpga interface
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <cstring> // memcpy()
+#include <type_traits>
+
+#include "compiler-gcc.h"
+#include "gtfpga_helpers.hpp"
+#include "gtfpga.cpp"
+
+const off_t PCIE_ADDRESS = get_pci_base_addr();
+
+int
+ACE_TMAIN(int argc, ACE_TCHAR *argv[])
+{
+  try {
+    // Initialize DomainParticipantFactory
+    DDS::DomainParticipantFactory_var dpf =
+      TheParticipantFactoryWithArgs(argc, argv);
+
+    // Create DomainParticipant
+    DDS::DomainParticipant_var participant =
+      dpf->create_participant(42,
+                              PARTICIPANT_QOS_DEFAULT,
+                              0,
+                              OpenDDS::DCPS::DEFAULT_STATUS_MASK);
+
+    if (!participant) {
+      ACE_ERROR_RETURN((LM_ERROR,
+                        ACE_TEXT("ERROR: %N:%l: main() -")
+                        ACE_TEXT(" create_participant failed!\n")),
+                       1);
+    }
+
+    // Register TypeSupport (EsmFeedbackSignals::FeedbackSignals)
+    EsmFeedbackSignals::FeedbackSignalsTypeSupport_var ts =
+      new EsmFeedbackSignals::FeedbackSignalsTypeSupportImpl;
+
+    if (ts->register_type(participant, "") != DDS::RETCODE_OK) {
+      ACE_ERROR_RETURN((LM_ERROR,
+                        ACE_TEXT("ERROR: %N:%l: main() -")
+                        ACE_TEXT(" register_type failed!\n")),
+                       1);
+    }
+
+    // Create Topic (Energy Storage Module Signals)
+    CORBA::String_var type_name = ts->get_type_name();
+    DDS::Topic_var topic =
+      participant->create_topic("Energy Storage Module Feedback Signals",
+                                type_name,
+                                TOPIC_QOS_DEFAULT,
+                                0,
+                                OpenDDS::DCPS::DEFAULT_STATUS_MASK);
+
+    if (!topic) {
+      ACE_ERROR_RETURN((LM_ERROR,
+                        ACE_TEXT("ERROR: %N:%l: main() -")
+                        ACE_TEXT(" create_topic failed!\n")),
+                       1);
+    }
+
+    // Create Publisher
+    DDS::Publisher_var publisher =
+      participant->create_publisher(PUBLISHER_QOS_DEFAULT,
+                                    0,
+                                    OpenDDS::DCPS::DEFAULT_STATUS_MASK);
+
+    if (!publisher) {
+      ACE_ERROR_RETURN((LM_ERROR,
+                        ACE_TEXT("ERROR: %N:%l: main() -")
+                        ACE_TEXT(" create_publisher failed!\n")),
+                       1);
+    }
+
+    // Create DataWriter
+    DDS::DataWriter_var writer =
+      publisher->create_datawriter(topic,
+                                   DATAWRITER_QOS_DEFAULT,
+                                   0,
+                                   OpenDDS::DCPS::DEFAULT_STATUS_MASK);
+
+    if (!writer) {
+      ACE_ERROR_RETURN((LM_ERROR,
+                        ACE_TEXT("ERROR: %N:%l: main() -")
+                        ACE_TEXT(" create_datawriter failed!\n")),
+                       1);
+    }
+
+    EsmFeedbackSignals::FeedbackSignalsDataWriter_var feedback_signals_writer =
+      EsmFeedbackSignals::FeedbackSignalsDataWriter::_narrow(writer);
+
+    if (!feedback_signals_writer) {
+      ACE_ERROR_RETURN((LM_ERROR,
+                        ACE_TEXT("ERROR: %N:%l: main() -")
+                        ACE_TEXT(" _narrow failed!\n")),
+                       1);
+    }
+
+    // Block until Subscriber is available
+    DDS::StatusCondition_var condition = writer->get_statuscondition();
+    condition->set_enabled_statuses(DDS::PUBLICATION_MATCHED_STATUS);
+
+    DDS::WaitSet_var ws = new DDS::WaitSet;
+    ws->attach_condition(condition);
+
+    while (true) {
+      DDS::PublicationMatchedStatus matches;
+      if (writer->get_publication_matched_status(matches) != ::DDS::RETCODE_OK) {
+        ACE_ERROR_RETURN((LM_ERROR,
+                          ACE_TEXT("ERROR: %N:%l: main() -")
+                          ACE_TEXT(" get_publication_matched_status failed!\n")),
+                         1);
+      }
+
+      if (matches.current_count >= 1) {
+        break;
+      }
+
+      DDS::ConditionSeq conditions;
+      DDS::Duration_t timeout = { 60, 0 };
+      if (ws->wait(conditions, timeout) != DDS::RETCODE_OK) {
+        ACE_ERROR_RETURN((LM_ERROR,
+                          ACE_TEXT("ERROR: %N:%l: main() -")
+                          ACE_TEXT(" wait failed!\n")),
+                         1);
+      }
+    }
+
+    ws->detach_condition(condition);
+
+    // Write samples
+    EsmFeedbackSignals::FeedbackSignals feedback_signals;
+	auto gtfpga = Gtfpga(PCIE_ADDRESS);
+	feedback_signals.name = "P1";
+	feedback_signals.isolation_status = 1;
+	feedback_signals.terminal_voltage_kV = gtfpga[0];
+	feedback_signals.terminal_current_kA = gtfpga[1];
+	feedback_signals.state_of_charge_pu = gtfpga[2];
+
+    for (int i = 0; i < 100; ++i) {
+      DDS::ReturnCode_t error = feedback_signals_writer->write(feedback_signals, DDS::HANDLE_NIL);
+	  feedback_signals.terminal_voltage_kV = gtfpga[0];
+	  feedback_signals.terminal_current_kA = gtfpga[1];
+	  feedback_signals.state_of_charge_pu = gtfpga[2];
+      usleep(500000);   //Not required for actual application
+
+      if (error != DDS::RETCODE_OK) {
+        ACE_ERROR((LM_ERROR,
+                   ACE_TEXT("ERROR: %N:%l: main() -")
+                   ACE_TEXT(" write returned %d!\n"), error));
+      }
+    }
+
+    // Wait for samples to be acknowledged
+    DDS::Duration_t timeout = { 30, 0 };
+    if (feedback_signals_writer->wait_for_acknowledgments(timeout) != DDS::RETCODE_OK) {
+      ACE_ERROR_RETURN((LM_ERROR,
+                        ACE_TEXT("ERROR: %N:%l: main() -")
+                        ACE_TEXT(" wait_for_acknowledgments failed!\n")),
+                       1);
+    }
+
+    // Clean-up!
+    participant->delete_contained_entities();
+    dpf->delete_participant(participant);
+
+    TheServiceParticipant->shutdown();
+
+  } catch (const CORBA::Exception& e) {
+    e._tao_print_exception("Exception caught in main():");
+    return 1;
+  }
+
+  return 0;
+}
