3018ea3d5227574dd303bd9c58d7788b49c6a723
Jerry Jean-Pierre <jeanpierrejerry@gmail.com>
2021-10-29 15:19:02 -0400

    Add files via upload

---
+++ EnergyManagementControlStandIn/emc.cpp
@@ -0,0 +1,38 @@
+/* EMC control stand in example */
+
+#include <stdio.h>
+#include <iostream>
+#include "rts.cpp"
+using namespace std;
+
+class EmcStandIn {
+
+private:
+    int total_signals;
+    Smio sm;
+
+public:
+
+    EmcStandIn()
+    {
+        total_signals = 128;
+        sm = Smio(total_signals);
+    }
+
+    EmcStandIn(size_t size)
+    {
+        total_signals = size;
+        sm = Smio(total_signals);
+    }
+
+    auto write_Value(float* signals, int& size)
+    {
+        sm.write_Signals(signals, size);
+    }
+
+    auto read_Value()
+    {
+        return sm.get_Base_Address();
+    }
+
+};
