d4574c6356b8deebda584d2812d40490c71f0b4b
Jerry Jean-Pierre <jonsejean@yahoo.com>
2022-02-14 03:40:47 -0500

    Remove time writing for command signals

--- emc_rtt/timeCalc.py
+++ emc_rtt/timeCalc.py
@@ -15,22 +15,7 @@ for pos in range(0,len(fb_time_list)-1):
     fb_write_file.write(str(fb_calc_time[pos]) + "\n")
 fb_write_file.close()
 
-cmd_read_file = open("rtt_command.txt", "r")
-cmd_write_file = open("command_times.txt", "w")
-
-cmd_time_list = []
-cmd_calc_time = []
-
-for str_time in cmd_read_file:
-    cmd_time_list.append(int(str_time))
-cmd_read_file.close()
-
-for pos in range(0,len(cmd_time_list)-1):
-    cmd_calc_time.append(cmd_time_list[pos+1] - cmd_time_list[pos])
-    cmd_write_file.write(str(cmd_calc_time[pos]) + "\n")
-cmd_write_file.close()
-
 #fig, axs = plt.subplots(1,1)
 plt.style.use('ggplot')
-plt.hist(fb_calc_time, bins=20)
+plt.hist(fb_calc_time, bins=100)
 plt.show()
